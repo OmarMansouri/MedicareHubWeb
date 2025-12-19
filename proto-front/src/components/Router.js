1000370021efec068c2f63fb443ada27b1b994cc
RXMO <omxrmxnsouri@gmail.com>
2025-12-19 12:50:09 +0100

    Fix the router

--- proto-front/src/components/Router.js
+++ proto-front/src/components/Router.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Sample from "./Sample";
 import Navbar from "./Navbar";
 import NotFound from "./NotFound";
+import Maps from "./Maps";
 
-export default function Router () {
-    return (
-        <BrowserRouter>
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<App />}/>
-                    <Route path="/sample" element={<Sample />}/>
-                    <Route path="*" element={<NotFound />}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
-};
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/sample" element={<Sample />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/maps" element={<Maps />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
