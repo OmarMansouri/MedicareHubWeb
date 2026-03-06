a61ccd979d48c3889b38720e5891d450d4725c48
Afrah KRAYNI <afrahkrayni@gmail.com>
2026-03-06 10:36:25 +0100

    correction ip 2

--- Medicare-front/src/modules/Antecedents/Api/AntecedentsApi.js
+++ Medicare-front/src/modules/Antecedents/Api/AntecedentsApi.js
@@ -1,5 +1,5 @@
 export function saveAntecedents(idPatient, diseaseIds, typeRelation) {
-return fetch("http://172.31.250.86:8081/antecedents/patient/" + idPatient, {
+return fetch("http://172.31.250.86:8081/antecedents/patient/" + idPatient, { // correction ip
 method: "POST",
  headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
